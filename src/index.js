07488e4504ad8c1fb20cdb8690d307c03d8191eb
heinop <pekka.heino@unikie.com>
2020-09-11 15:37:56 +0300

    step1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Title = ({ text }) => {
+  return (
+    <h2>{text}</h2>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const Result = ({ label, value }) => {
+  return (
+    <>
+      <span>{label} {value}</span>
+      <br />
+    </>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Title text='give feedback' />
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <Title text='statistics' />
+      <Result label='good' value={good} />
+      <Result label='neutral' value={neutral} />
+      <Result label='bad' value={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
